0f62890f9b7a72dca9b4e90399fd2e0d2c406bbe
KoRIOz675 <thomas@mafille.net>
2024-01-07 01:27:41 +0100

    MAJOR: Refait depuis la question 6 a 15

--- Projet_SSII.m
+++ Projet_SSII.m
@@ -25,12 +25,12 @@ Te1 = t(2)-t(1);
 
 %% Question 4
 Te2= 0.05; 
-Fe2=1/Te2;
+Fe1=1/Te1;
 Tf=t(end);
-N=Tf/Te2 ;
+N=Tf/Te1;
 
-f1=-Fe2*(N/2-1)/N:Fe2/N:0;
-f2=Fe2/N:Fe2/N:(N/2)*Fe2/N;
+f1=-Fe1*(N/2-1)/N:Fe1/N:0;
+f2=Fe1/N:Fe1/N:(N/2)*Fe1/N;
 f = [f2,f1];
 w= zeros(N,1);
 for m=1:N
@@ -43,119 +43,161 @@ end
 figure(2)
 stem(f,abs(w)/N)
 grid on
-hold on
 xlim([-2 2])
 xlabel('f [Hz]')
-ylabel('DFT(\omega (t))')
+ylabel('Amplitude Spectrum of Angular Speed Signal')
 
 
 %% Question 6
 % filter design
-t1=0:Te2:t(end)-Te2;
-fc1=2;
-H1=tf(1,[1/(2*pi*fc1)  1]);
-wf=lsim(H1,w,t1);
+t1=0:Te1:t(end)-Te1;
+fc=0.2;
+wc=2*pi*fc;
+
+H1=tf(1,[1/(2*pi*fc)  1]);
+wf=lsim(H1,omega,t);
 
 % plot of filtered signal
 figure(1);
-plot(t1,wf,'r')
+plot(t,wf,'r')
+hold off
 grid on
-legend(' \omega(t) unfiltered','\omega_{f}(t) filtered','Fontsize',14)
+legend(' omega(t) unfiltered','omega_{f}(t) filtered','Fontsize',14)
 
-t_temp = t';
-omega = omega';
-data = [t_temp,omega];
+data = [t',omega'];
 
 
 %% Question 7
-figure(2)
-stem(f,abs(wf)/N, 'r')
+wf1=zeros(N,1);
+for m = 1 : N
+    for k = 1 : N
+        wf1(m) = wf1(m) + wf(k) * exp(-1i*2*pi*m*k/N);
+    end
+end
+
+figure(3)
+subplot(2,1,1);
+stem(f,abs(w)/N), hold on
+stem(f,abs(wf1)/N, 'r'), hold off
+grid on
+xlim([-100 100])
+legend(' DFT of omega(t)','DFT of omega_{f}(t)','Fontsize',14)
+title('DFT(w(t) and DFT(w_{f}(t) within [-100,100] Hz')
+
+subplot(2,1,2);
+stem(f,abs(w)/N), hold on
+stem(f,abs(wf1)/N, 'r'), hold off
 grid on
 xlim([-2 2])
-legend(' DFT of \omega(t)','DFT of \omega_{f}(t)','Fontsize',14)
+legend(' DFT of omega(t)','DFT of omega_{f}(t)','Fontsize',14)
+title('DFT(w(t) and DFT(w_{f}(t) within [-2,2] Hz')
 
 
 
 %% Sampling
 
-% Question 8
-temp1 = 1:round(Te2/Te1):length(wf);
+% Question 8 and 10
+temp1 = 1:round(Te2/Te1):length(t);
 we=wf(temp1);
+Te = t(temp1);
 
-%% Question 10
-Te = (0:length(we)-1) * Te2;
 
 %% Question 11
-figure(3)
-plot(t1,wf)
+figure(4)
+plot(t,wf), hold on
 xlim([10 12])
 xlabel('Time [sec]')
 ylabel('Angular Velocity [rad/sec]')
 grid on
-hold on
-stem(Te,abs(we), 'r')
-legend(' \omega_{f}(t)','\omega_{e}(t)','Fontsize',14)
+stem(Te,we, 'r'), hold off
+legend(' omega_{f}(t)','omega_{e}(t)','Fontsize',14)
 
 %% Question 12
-we_dft = fft(we);
+Fe2=1/Te2;
+Tf2=Te(end);
+N2=Tf2/Te2;
 
-figure(4)
-stem(f,abs(w)/N)
-hold on
-stem(f,abs(wf)/N, 'r')
-stem(Te,abs(we_dft)/N, 'g')
-xlabel('Frequency [Hz]')
-ylabel('Amplitude')
-legend({'ω(t)', 'ωf(t)', 'ωe(t)'})
-xlim([-1 1])
+f3=-Fe2*(N2/2-1)/N2:Fe2/N2:0;
+f4=Fe2/N2:Fe2/N2:(N2/2)*Fe2/N2;
+f_2=[f4,f3];
 
-%% Questions 13  
-wd=zeros(8,1);
-j=0;
-
-TeM=Te';
-
-wd(1)=(we(1))/TeM(1);
-for i=2:7
-    wd(i)=(we(i)-we(i-1))/(2*TeM(i));
+we_dft = zeros(N2,1);
+for m = 1 : N2
+    for k = 1 : N2
+        we_dft(m) = we_dft(m) + we(k) * exp(-1i*2*pi*m*k/N2);
+    end
 end
-wd(8)=(we(8)-we(7))/TeM(8);
 
-%theta=Te*sigma(we(i))
+figure(5)
+stem(f,abs(w)/N), hold on
+stem(f,abs(wf1)/N, 'r')
+stem(f_2,abs(we_dft)/N2, 'g'), hold off
+xlabel('Frequency [Hz]')
+ylabel('DFT')
+legend({'DFT of omega(t)', 'DFT of omega_f(t)', 'DFT of omega_e(t)'})
+xlim([-2 2])
 
-theta=zeros(8,1);
-sum=0;
+%% Questions 13
+% angular acceleration
+wd_start=(we(2)-we(1))/Te2;
+wd_end=(we(end)-we(end-1))/Te2;
+wd_mid=zeros(8,1);
+for i=2:N2-1
+    wd_mid(i)=(we(i+1)-we(i-1))/(2*Te2);
+end
 
-for i=1:8
-    sum=sum+we(i);
-    theta(i)=TeM(i)*sum;
+wd=[wd_start;wd_mid;wd_end];
+
+
+% angular position
+theta=zeros(N2,1);
+
+for i=1:N2
+    for k=1:i+2
+        theta(i)=theta(i)+Te2*we(k);
+    end
 end
 
 %% Question 14
-figure(5)
-plot(Te, theta)
-xlabel('Time [sec]')
-ylabel('Position')
-grid on
+t_ang=0:Te2:Te(end)-Te2;
 
 figure(6)
+plot(t_ang, theta)
+xlabel('Time [sec]')
+ylabel('Angular position [rad]')
+grid on
+
+figure(7)
 plot(Te,wd)
 xlabel('Time [sec]')
-ylabel('Acceleration')
+ylabel('Angular acceleration [rad/s^2]')
 grid on
 
 %% Question 15
-theta_dft = fft(theta);
-wd_dft = fft(wd);
+wd_dft=zeros(N2,1);
+for m=1:N2
+    for k=1:N2
+        wd_dft(m)=wd_dft(m)+wd(k)*exp(-1i*2*pi*m*k/N2);
+    end
+end
 
-figure(7)
-stem(Te,abs(theta_dft)/N)
-grid on
-xlabel('Frequency [Hz]')
-ylabel('DFT of theta')
+theta_dft = zeros(N2,1);
+for m=1:N2
+    for k=1:N2
+        theta_dft(m)=theta_dft(m)+theta(k)*exp(-1i*2*pi*m*k/N2);
+    end
+end
 
 figure(8)
-stem(Te,wd_dft)
+stem(f_2,abs(theta_dft)/N2)
+xlim([-2 2])
 grid on
 xlabel('Frequency [Hz]')
-ylabel('DFT of omega dot')
+ylabel('DFT of theta(t)')
+
+figure(9)
+stem(f_2,abs(wd_dft)/N2)
+xlim([-2 2])
+grid on
+xlabel('Frequency [Hz]')
+ylabel("DFT of omega'(t)")
